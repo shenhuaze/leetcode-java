77d5c9ff2c372178b41004a86b7f8386f10b4189
shenhuaze <shenhuaze@pku.edu.cn>
2019-07-25 21:26:13 +0800

    update Permutations.java

--- src/main/java/com/huaze/shen/Permutations.java
+++ src/main/java/com/huaze/shen/Permutations.java
@@ -10,6 +10,9 @@ import java.util.Set;
  * @date 2019-07-24
  */
 public class Permutations {
+    /**
+     * Solution 1
+     */
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> results = new ArrayList<>();
         if (nums == null || nums.length == 0) {
@@ -38,8 +41,40 @@ public class Permutations {
         }
     }
 
+    /**
+     * Solution 2
+     */
+    public List<List<Integer>> permute2(int[] nums) {
+        List<List<Integer>> results = new ArrayList<>();
+        if (nums == null || nums.length == 0) {
+            return results;
+        }
+        List<Integer> permutation = new ArrayList<>();
+        int[] visited = new int[nums.length];
+        helper2(results, permutation, nums, visited);
+        return results;
+    }
+
+    private void helper2(List<List<Integer>> results, List<Integer> permutation, int[] nums, int[] visited) {
+        if (permutation.size() == nums.length) {
+            results.add(new ArrayList<>(permutation));
+            return;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (visited[i] == 1) {
+                continue;
+            }
+            permutation.add(nums[i]);
+            visited[i] = 1;
+            helper2(results, permutation, nums, visited);
+            visited[i] = 0;
+            permutation.remove(permutation.size() - 1);
+        }
+    }
+
     public static void main(String[] args) {
         int[] nums = {1, 2, 3};
-        System.out.println(new Permutations().permute(nums));
+        //System.out.println(new Permutations().permute(nums));
+        System.out.println(new Permutations().permute2(nums));
     }
 }
