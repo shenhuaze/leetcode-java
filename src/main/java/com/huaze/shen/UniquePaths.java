4ab090b68b1f67c5c0fab3791c9b1e1d63cb0584
shenhuaze <shenhuaze@pku.edu.cn>
2019-08-17 23:08:28 +0800

    add UniquePaths.java

---
+++ src/main/java/com/huaze/shen/UniquePaths.java
@@ -0,0 +1,55 @@
+package com.huaze.shen;
+
+/**
+ * @author Huaze Shen
+ * @date 2019-08-17
+ */
+public class UniquePaths {
+    /**
+     * Solution 1
+     */
+    public int uniquePaths(int m, int n) {
+        if (m <= 0 || n <= 0) {
+            return 0;
+        }
+        int[][] dp = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            dp[i][0] = 1;
+        }
+        for (int i = 0; i < n; i++) {
+            dp[0][i] = 1;
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+
+    /**
+     * Solution 2
+     */
+    public int uniquePaths2(int m, int n) {
+        if (m <= 0 || n <= 0) {
+            return 0;
+        }
+        int[] dp = new int[n];
+        for (int i = 0; i < n; i++) {
+            dp[i] = 1;
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[j] += dp[j - 1];
+            }
+        }
+        return dp[n - 1];
+    }
+
+    public static void main(String[] args) {
+        int m = 3;
+        int n = 2;
+        System.out.println(new UniquePaths().uniquePaths(m, n));
+        System.out.println(new UniquePaths().uniquePaths2(m, n));
+    }
+}
