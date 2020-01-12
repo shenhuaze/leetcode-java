bd76ffdb88def0061782019a095b9af8d0583db2
shenhuaze <shenhuaze@pku.edu.cn>
2020-01-12 14:15:27 +0800

    add SymmetricTree.java

---
+++ src/main/java/com/huaze/shen/SymmetricTree.java
@@ -0,0 +1,47 @@
+package com.huaze.shen;
+
+/**
+ * @author Huaze Shen
+ * @date 2020-01-12
+ */
+public class SymmetricTree {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        return isSymmetric(root.left, root.right);
+    }
+
+    private boolean isSymmetric(TreeNode left, TreeNode right) {
+        if (left == null && right == null) {
+            return true;
+        } else if (left == null || right == null) {
+            return false;
+        } else if (left.val != right.val) {
+            return false;
+        }
+        return isSymmetric(left.right, right.left) && isSymmetric(left.left, right.right);
+    }
+
+    public void demo() {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(2);
+        root.left.left = new TreeNode(3);
+        root.right.right = new TreeNode(3);
+        root.left.right = new TreeNode(4);
+        root.right.left = new TreeNode(4);
+        System.out.println(isSymmetric(root));
+    }
+
+    public static void main(String[] args) {
+        new SymmetricTree().demo();
+    }
+}
