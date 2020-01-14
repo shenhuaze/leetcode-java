cb078d5d65344be6f5c0443f68a7c03b5a75de63
shenhuaze <shenhuaze@pku.edu.cn>
2020-01-14 08:24:52 +0800

    add BinaryTreeLevelOrderTraversal.java

---
+++ src/main/java/com/huaze/shen/BinaryTreeLevelOrderTraversal.java
@@ -0,0 +1,61 @@
+package com.huaze.shen;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * @author Huaze Shen
+ * @date 2020-01-13
+ */
+public class BinaryTreeLevelOrderTraversal {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> results = new ArrayList<>();
+        if (root == null) {
+            return results;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            List<Integer> levelList = new ArrayList<>();
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                if (node == null) {
+                    continue;
+                }
+                levelList.add(node.val);
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+            results.add(levelList);
+        }
+        return results;
+    }
+
+    public void demo() {
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(9);
+        root.right = new TreeNode(20);
+        root.right.left = new TreeNode(15);
+        root.right.right = new TreeNode(7);
+        List<List<Integer>> results = levelOrder(root);
+        System.out.println(results);
+    }
+
+    public static void main(String[] args) {
+        new BinaryTreeLevelOrderTraversal().demo();
+    }
+}
