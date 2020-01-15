41b3292a6a245fd004955137f397bb640b2c8ab5
shenhuaze <shenhuaze@pku.edu.cn>
2020-01-15 08:45:11 +0800

    add MaximumDepthOfBinaryTree.java

---
+++ src/main/java/com/huaze/shen/MaximumDepthOfBinaryTree.java
@@ -0,0 +1,34 @@
+package com.huaze.shen;
+
+/**
+ * @author Huaze Shen
+ * @date 2020-01-15
+ */
+public class MaximumDepthOfBinaryTree {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+    }
+
+    public void demo() {
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(9);
+        root.right = new TreeNode(20);
+        root.right.left = new TreeNode(15);
+        root.right.right = new TreeNode(7);
+        System.out.println(maxDepth(root));
+    }
+
+    public static void main(String[] args) {
+        new MaximumDepthOfBinaryTree().demo();
+    }
+}
