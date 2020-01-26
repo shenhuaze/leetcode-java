e6eb62053140fcac84fca8d28036bdf4a8dcd22e
shenhuaze <shenhuaze@pku.edu.cn>
2020-01-26 16:46:31 +0800

    add MinimumDepthOfBinaryTree.java

---
+++ src/main/java/com/huaze/shen/MinimumDepthOfBinaryTree.java
@@ -0,0 +1,40 @@
+package com.huaze.shen;
+
+/**
+ * @author Huaze Shen
+ * @date 2020-01-26
+ */
+public class MinimumDepthOfBinaryTree {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+
+    public int minDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        if (root.left == null) {
+            return minDepth(root.right) + 1;
+        }
+        if (root.right == null) {
+            return minDepth(root.left) + 1;
+        }
+        return Math.min(minDepth(root.left), minDepth(root.right)) + 1;
+    }
+
+    public void demo() {
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(9);
+        root.right = new TreeNode(20);
+        root.right.left = new TreeNode(15);
+        root.right.right = new TreeNode(7);
+        System.out.println(minDepth(root));
+    }
+
+    public static void main(String[] args) {
+        new MinimumDepthOfBinaryTree().demo();
+    }
+}
