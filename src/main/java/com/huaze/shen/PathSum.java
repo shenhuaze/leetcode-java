439840897788a53b98d61b736fd4daa2c08f6bb1
shenhuaze <shenhuaze@pku.edu.cn>
2020-01-27 13:44:03 +0800

    add PathSum.java

---
+++ src/main/java/com/huaze/shen/PathSum.java
@@ -0,0 +1,42 @@
+package com.huaze.shen;
+
+
+/**
+ * @author Huaze Shen
+ * @date 2020-01-27
+ */
+public class PathSum {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if (root == null) {
+            return false;
+        }
+        if (root.left == null && root.right == null && root.val == sum) {
+            return true;
+        }
+        return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
+    }
+
+    public void demo() {
+        TreeNode root = new TreeNode(5);
+        root.left = new TreeNode(4);
+        root.left.left = new TreeNode(11);
+        root.left.left.left = new TreeNode(7);
+        root.left.left.right = new TreeNode(2);
+        root.right = new TreeNode(8);
+        root.right.left = new TreeNode(13);
+        root.right.right = new TreeNode(4);
+        root.right.right.right = new TreeNode(1);
+        System.out.println(hasPathSum(root, 22));
+    }
+
+    public static void main(String[] args) {
+        new PathSum().demo();
+    }
+}
